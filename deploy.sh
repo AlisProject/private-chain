2c58575c7250b0e7b3c055761f622bf722853989
sot528 <sot528@gmail.com>
2018-05-13 19:31:27 +0900

    add delete on termination flag to env valuables

--- deploy.sh
+++ deploy.sh
@@ -6,6 +6,7 @@ aws cloudformation deploy \
     ParityNodesAMI=${PARITY_NODES_AMI} \
     BastionAllocationId=${BASTION_ALLOCATION_ID} \
     NatAllocationId=${NAT_ALLOCATION_ID} \
+    EC2DeleteOnTermination=${EC2_DELETE_ON_TERMINATION} \
     AccountsNewRequestPassword=${ACCOUNTS_NEW_REQUEST_PASSWORD} \
     ParityNodesInstanceType=${PARITY_NODES_INSTANCE_TYPE} \
     ParityNodesVolumeSize=${PARITY_NODES_VOLUME_SIZE} \
